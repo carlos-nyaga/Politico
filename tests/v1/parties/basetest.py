30d7d40b49a0cf661dfcd679bebf2b5d31a5335f
4LG0R <crlsnyaga@gmail.com>
2019-02-08 15:33:20 +0300

    Fixed import errors in test unit

--- tests/v1/parties/basetest.py
+++ tests/v1/parties/basetest.py
@@ -7,22 +7,16 @@ from app import create_app
 
 class BaseTest(unittest.TestCase):
     """ Base Class for testing Parties Endpoint """
-
-    def payload(self):
-        return {
-            "name": self.name,
-            "hqAddress": self.hqaddress,
-            "logoUrl": self.logourl
-        }
-    
+        
     expected_status_code = 200
     expected_return_payload = {}
+    data = {}
 
 
     def setUp(self):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
-        self.response = self.client.post('/api/v1{}'.format(self.path), headers={'Content-Type':'application/json', data=json.dumps(payload)})
+        self.response = self.client.post('/api/v1/parties', headers={'Content-Type':'application/json'}, data=json.dumps(self.data))
         
     def test_should_return_expected_status_code(self):
         self.assertEqual(self.response.status, self.expected_status_code)
